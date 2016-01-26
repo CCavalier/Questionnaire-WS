fd3e511fc0f2a46b9339b9deea37ab01318cccd5
Charlotte Cavalier <malika@localhost.localdomain>
2016-01-26 23:51:52 +0100

    [CCR] methode de validation du fichier passe en parametre !-- TODO -- Enrichir le format d' exception

--- Questionnaire-WS/src/test/java/features/QuestionnaireReadingSteps.java
+++ Questionnaire-WS/src/test/java/features/QuestionnaireReadingSteps.java
@@ -1,12 +1,13 @@
 package features;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Assert;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.CollectionUtils;
 
+import com.cavalier.questionnaire.back.exception.IncorrectFileException;
 import com.cavalier.questionnaire.back.model.Question;
 import com.cavalier.questionnaire.back.service.reader.ExcelReader;
 import com.cavalier.questionnaire.back.utils.QuestionnaireValidator;
@@ -22,29 +23,46 @@ public class QuestionnaireReadingSteps {
 	
 	private List<Question>  questionsList = new ArrayList<Question>();
 	
-	private String fileUrl ;
+	private Exception exceptionCatched;
+
+	private URL fileUrl;
+
 	
 	@Given("^an properly formatted excel file \"([^\"]*)\"$")
-	public void an_properly_formatted_excel_file(String fileUrl) throws Throwable {
-	    QuestionnaireValidator.validateFileFormat(fileUrl);
+	public void an_properly_formatted_excel_file(String url) throws Throwable {
+		URL completeUrl = this.getClass().getClassLoader().getResource(url);
+	    QuestionnaireValidator.validateFileFormat(completeUrl.getPath());
+	    this.fileUrl = completeUrl;
 	    
 	}
 
+	@Given("^an poorly formatted excel file \"([^\"]*)\"$")
+	public void an_poorly_formatted_excel_file(String url) throws Throwable {
+		URL completeUrl = this.getClass().getClassLoader().getResource(url);
+		try{
+			QuestionnaireValidator.validateFileFormat(completeUrl.getPath());
+		}catch(IncorrectFileException e){
+			exceptionCatched = e;
+		}
+	}
 
 	@When("^the user import the file$")
 	public void the_user_import_the_file() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-//	    throw new PendingException();
-/*		fileUrl = this.getClass().getClassLoader().getResource("Questionnaire.csv").getPath();
-		System.out.println("file : "+fileUrl);*/
-		questionsList = excelReader.parseFile("/home/malika/sources/Questionnaire/Questionnaire-WS/src/test/resources/Questionnaire.csv");
-		Assert.assertNotNull(questionsList);
+		if(null == exceptionCatched){
+			questionsList = excelReader.parseFile(fileUrl.getPath());
+			Assert.assertNotNull(questionsList);
+		}		
 	}
 
 	@Then("^the questions are correctly imported$")
 	public void the_questions_are_correctly_imported() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 	    Assert.assertFalse(CollectionUtils.isEmpty(questionsList));
+	}
+	
+	@Then("^an incorrect format exception is thrown$")
+	public void an_incorrect_format_exception_is_thrown() throws Throwable {
+	    Assert.assertTrue(exceptionCatched instanceof IncorrectFileException);
 	}
 	
 }
