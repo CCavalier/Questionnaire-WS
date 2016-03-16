8fea75ee5a88ee41ce5f65fcdbfb76fa21c72385
Charlotte Cavalier <malika@localhost.localdomain>
2016-03-16 22:46:55 +0100

    '#yolobis

---
+++ src/main/java/com/cavalier/questionnaire/back/utils/ExcelFileColumns.java
@@ -0,0 +1,7 @@
+package com.cavalier.questionnaire.back.utils;
+
+public class ExcelFileColumns {
+	
+	public static final String[] EXCEL_COLUMNS = {"Theme","Question","choix 1","choix 2","choix 3","choix 4","reponse"};
+
+}
