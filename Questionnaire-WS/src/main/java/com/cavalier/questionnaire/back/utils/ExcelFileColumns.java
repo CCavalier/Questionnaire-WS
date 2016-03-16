5917c08179428a2b89e02b1e1b1711ab078a3d18
Charlotte Cavalier <malika@localhost.localdomain>
2016-03-16 22:44:21 +0100

    '#yolo

--- Questionnaire-WS/src/main/java/com/cavalier/questionnaire/back/utils/ExcelFileColumns.java
+++
@@ -1,7 +0,0 @@
-package com.cavalier.questionnaire.back.utils;
-
-public class ExcelFileColumns {
-	
-	public static final String[] EXCEL_COLUMNS = {"Theme","Question","choix 1","choix 2","choix 3","choix 4","reponse"};
-
-}
